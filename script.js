61f5fb8dcc1c4cb4dfa34b0206b3a1d2a3d1c452
Zain77-sari <202303729@student.polytechnic.bh>
2025-10-19 17:37:09 +0300

    Add form submission and table row click alerts

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.getElementById("appointmentform").onsubmit = function () {  
+  alert("Your inquiry has been submitted. ");  
+  return false;   
+};  
+document.getElementsByTagName("tr")[1].onclick = function () { 
+  alert(" SoldOut"); 
+}; 
+ 
 
 
 
